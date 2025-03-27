0ece576df4aa09216b25d0b7c0f0540d88e5df49
Juan Manuel <>
2025-03-27 14:30:54 +0100

    [initial] creating project

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by Juan Manuel de la Cruz on 27/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CityWeather
+//
+//  Created by Juan Manuel de la Cruz on 27/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
