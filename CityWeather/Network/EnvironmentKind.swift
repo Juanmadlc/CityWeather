23a3cfbcc3e29a681268784a9487c59ae6011deb
Juan Manuel <>
2026-03-06 17:09:56 +0100

    [Network] Add EnvironmentKind and template comments

---
+++ CityWeather/Network/EnvironmentKind.swift
@@ -0,0 +1,13 @@
+//
+//  EnvironmentKind.swift
+//  CityWeather
+//
+//  Created by Juan Manuel de la Cruz on 6/3/26.
+//
+
+import Foundation
+
+enum EnvironmentKind: String, CaseIterable, Codable {
+    case dev
+    case pro
+}
