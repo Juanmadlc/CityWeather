a219f45bb10f24b0f8a75c5c8eddeca520b45029
Juan Manuel <>
2026-07-22 13:28:28 +0200

    [Dashboard]  onchange	add

--- CityWeather/Platform/Scenes/Dashboard/View/DashboardView.swift
+++ CityWeather/Platform/Scenes/Dashboard/View/DashboardView.swift
@@ -37,6 +37,15 @@ struct DashboardView<ViewModel>: View where ViewModel: DashboardViewModelProtoco
         .task {
             await viewModel.fetchDataWeather(city: city)
         }
+        
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .active {
+                Task {
+                    await viewModel.fetchDataWeather(city: city)
+                }
+            }
+        }
+        
     }
 }
 
