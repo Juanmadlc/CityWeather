bbecabba0cd6325c2ce973e2cc3af1d46dbd2b23
Juan Manuel <>
2026-07-28 15:10:03 +0200

    [Dashboard]  mapservices apikey

---
+++ CityWeather/Network/MapServices/MapServicesApiKey.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct MapServicesAPIConfig {
+    static let appid = "768543f20334a8c0ab4d96b800f607e5"
+}
--- CityWeather/Network/MapServices/MapServicesEndpoints.swift
+++ CityWeather/Network/MapServices/MapServicesEndpoints.swift
@@ -21,7 +21,7 @@ struct MapServicesEndpoints {
     static let dataForecast = "/data/2.5/forecast"
     
     // MARK: PARAMS
-    static let appid = "768543f20334a8c0ab4d96b800f607e5"
+    static let appid = MapServicesAPIConfig.appid
     static let units = "metric"
     static var language: String {
         guard let preferredLanguage = Locale.preferredLanguages.first,
