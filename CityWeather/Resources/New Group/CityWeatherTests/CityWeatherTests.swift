10d529f3a3e7ba131179e403eecda4d04fa68de4
Juan Manuel <>
2026-09-08 17:06:40 +0200

    [Dashboard]   add unit test UserDefaultsStorageTest..	..

---
+++ CityWeather/Resources/New Group/CityWeatherTests/CityWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherTests.swift
+//  CityWeatherTests
+//
+//  Created by Juan Manuel de la Cruz on 27/3/25.
+//
+
+import Testing
+@testable import CityWeather
+
+struct CityWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
